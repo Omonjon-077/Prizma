633fa98df2178df775904030b44c128f243b1560
Omonjon <omonjons077@gmail.com>
2023-10-07 11:50:11 +0500

    Send tg bot and modal modified

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,19 @@
 let form = document.querySelector("#contact-form");
 let contactName = document.querySelector("#contact-name");
-let contactPhone = document.querySelector("#contact-phone");
 let modal = document.getElementById("modal-info");
 let modalBtn =document.getElementById("modal-btn");
 
+// FOR INPUT MASK
+const contactPhone = document.getElementById('contact-phone');
+const maskOptions = {
+    mask: '+{998}(00)000-00-00'
+};
+const mask = IMask(contactPhone, maskOptions);
+
+// FOR SEND BOT
 let bot = {
     TOKEN: "5894687462:AAFNdK9xm8LFIsSxiq9BL951WwznTfxVVX0",
-    chatID: "-1001588367236",
+    chatID: "-4016506469",
 }
 
 form.addEventListener("submit", (e) => {
@@ -21,16 +28,14 @@ form.addEventListener("submit", (e) => {
         method: "GET"
     })
         .then(success => {
-            modal.classList.remove("d-none");
-            modal.classList.add("d-block");
+            modal.classList.add("show-modal");
             setTimeout(() => {
-                modal.classList.remove("d-block");
-                modal.classList.add("d-none");
+                modal.classList.remove("show-modal");
             }, 5000)
             contactName.value = "";
             contactPhone.value = "";
             modalBtn.addEventListener("click", () => {
-                modal.classList.add("d-none");
+                modal.classList.remove("show-modal");
             })
         }, error => {
             alert("Message not send!");
